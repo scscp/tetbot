9321bf4db9376902b25f1936dcb70a965ad61b68
scscp <143437421+scscp@users.noreply.github.com>
2023-08-31 22:16:46 +0700

    Update port-tr.sh

--- trojan/port-tr.sh
+++ trojan/port-tr.sh
@@ -39,7 +39,7 @@ fi
 
 PERMISSION () {
     MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/Mahfud2128/tetbot/main/skkkk | awk '{print $4}' | grep $MYIP)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/scscp/tetbot/main/skkkk | awk '{print $4}' | grep $MYIP)
     if [ "$MYIP" = "$IZIN" ]; then
     Bloman
     else
