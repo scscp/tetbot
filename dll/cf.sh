fa64c1311960111c313e9dabcee9eb11e199505f
scscp <143437421+scscp@users.noreply.github.com>
2023-09-01 00:12:55 +0700

    Update cf.sh

--- dll/cf.sh
+++ dll/cf.sh
@@ -4,7 +4,7 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
 
 BURIQ () {
-    curl -sS https://raw.githubusercontent.com/Mahfud2128/tetbot/main/skkkk > /root/tmp
+    curl -sS https://raw.githubusercontent.com/scscp/tetbot/main/skkkk > /root/tmp
     data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
     for user in "${data[@]}"
     do
@@ -22,7 +22,7 @@ BURIQ () {
 }
 
 MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS https://raw.githubusercontent.com/Mahfud2128/tetbot/main/skkkk | grep $MYIP | awk '{print $2}')
+Name=$(curl -sS https://raw.githubusercontent.com/scscp/tetbot/main/skkkk | grep $MYIP | awk '{print $2}')
 echo $Name > /usr/local/etc/.$Name.ini
 CekOne=$(cat /usr/local/etc/.$Name.ini)
 
@@ -39,7 +39,7 @@ fi
 
 PERMISSION () {
     MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/Mahfud2128/tetbot/main/skkkk | awk '{print $4}' | grep $MYIP)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/scscp/tetbot/main/skkkk | awk '{print $4}' | grep $MYIP)
     if [ "$MYIP" = "$IZIN" ]; then
     Bloman
     else
@@ -70,9 +70,9 @@ fi
 
 source /root/scdomain
 SUB_DOMAIN=$peler
-DOMAIN=regsub.cf
-CF_ID=irwan@aidan.my
-CF_KEY=9531fde17724c8f2b297980c00498bc359e80
+DOMAIN=kingstore.biz.id
+CF_ID=irawancandra6699@aidan.my
+CF_KEY=b07ea594ebc844a5d9db4a88fbdb09d9a7170
 set -euo pipefail
 IP=$(curl -sS ifconfig.me);
 echo "Updating DNS for ${SUB_DOMAIN}..."
